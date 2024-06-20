71cff0a20c86c4d3bb8bd4660352cfb050176017
Good <lzs1999@gmail.com>
2024-06-20 11:48:09 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -13,6 +13,7 @@ type Config struct {
 	Reject              bool // 不显示指明Reject则直接drop
 	RateTrigger         string
 	ReverseProxySupport bool // 由于X-FORWARD-FOR 请求头可以任意伪造，所以反向代理支持仅作为可选项
+	Interactive         bool // 是否启用交互式检测stdin响应用户输入的命令
 }
 
 var ServiceConfig *Config
